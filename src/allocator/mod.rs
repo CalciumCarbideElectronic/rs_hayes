21c61bb7ef4f9c0cd7861e38c93a7848228c8701
CNLHC <2463765697@qq.com>
2020-01-13 15:25:20 +0800

    naive allocator

---
+++ src/allocator/mod.rs
@@ -0,0 +1,19 @@
+use alloc_cortex_m::CortexMHeap;
+use alloc::fmt::format;
+use crate::cffi::import;
+
+#[cfg(not(test))]
+#[global_allocator]
+pub static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+
+#[cfg(not(test))]
+#[alloc_error_handler]
+fn my_example_handler(layout: core::alloc::Layout) -> ! {
+    let s =  alloc::string::String::from(
+        format!("memory allocation of {} bytes failed", layout.size())
+    );
+    unsafe{
+        import::uart_send(s.as_bytes().as_ptr(),s.len());
+    }
+    panic!("memory allocation of {} bytes failed", layout.size())
+}
