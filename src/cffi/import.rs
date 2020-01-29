d545dd83a6520b49ea141aeff2e9d2a3b99eedce
CNLHC <2463765697@qq.com>
2020-01-29 22:36:48 +0800

    import more function from c cide

--- src/cffi/import.rs
+++ src/cffi/import.rs
@@ -1,16 +1,28 @@
-
+use alloc::string::String;
 #[cfg(not(test))]
 extern {
-    pub fn uart_send(data: *const  u8, size: usize)->i32;
-    pub fn strlen(p: *const u8)->u32;
+    pub fn uart_send(data: *const u8, size: usize) -> i32;
+    pub fn Debug(data: *const u8);
+    pub fn strlen(p: *const u8) -> u32;
+    pub fn osDelay(tick: usize);
+}
+
+pub unsafe fn DebugS(s: String) {
+    Debug(s.as_str().as_ptr());
 }
 
 #[cfg(test)]
-pub fn uart_send(data: *const  u8, size: usize)->i32{
+pub fn uart_send(data: *const u8, size: usize) -> i32 {
     0
 }
 #[cfg(test)]
-extern{
-    pub fn strlen(p: *const u8)->u32;
+use std::{thread, time};
 
+#[cfg(test)]
+pub fn osDelay(tick: usize) {
+    thread::sleep(time::Duration::from_millis(2));
+}
+#[cfg(test)]
+extern "C" {
+    pub fn strlen(p: *const u8) -> u32;
 }
