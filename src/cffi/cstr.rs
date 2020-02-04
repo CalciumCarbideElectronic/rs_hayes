c851fb445b10af8891828ec743fdc0ee72dd5b56
CNLHC <2463765697@qq.com>
2020-02-04 23:48:52 +0800

    fix bug: standard strlen can not detect string.as_str() directly

--- src/cffi/cstr.rs
+++ src/cffi/cstr.rs
@@ -12,35 +12,4 @@ impl CStr {
         let len = strlen(ptr) as usize;
         str::from_utf8_unchecked(slice::from_raw_parts(ptr, len))
     }
-
-    pub unsafe fn len(&self) -> usize {
-        let bytes = self.to_bytes_with_nul();
-        bytes.len()
-    }
-
-    pub unsafe fn from_ptr<'a>(ptr: *const u8) -> &'a CStr {
-        let len = strlen(ptr);
-        let ptr = ptr as *const u8;
-        CStr::from_bytes_with_nul_unchecked(slice::from_raw_parts(ptr, len as usize + 1))
-    }
-
-    #[inline]
-    const unsafe fn from_bytes_with_nul_unchecked(bytes: &[u8]) -> &CStr {
-        &*(bytes as *const [u8] as *const CStr)
-    }
-
-    #[inline]
-    pub fn to_bytes(&self) -> &[u8] {
-        let bytes = self.to_bytes_with_nul();
-        &bytes[..bytes.len() - 1]
-    }
-
-    #[inline]
-    pub fn to_bytes_with_nul(&self) -> &[u8] {
-        unsafe { &*(&self.inner as *const [u8] as *const [u8]) }
-    }
-
-    pub unsafe fn to_str_unsafe(&self) -> &str {
-        str::from_utf8_unchecked(self.to_bytes())
-    }
 }
--- src/cffi/import.rs
+++ src/cffi/import.rs
@@ -1,21 +1,22 @@
 use alloc::string::String;
+use stdi
 #[cfg(not(test))]
 extern {
     pub fn uart_send(data: *const u8, size: usize) -> i32;
-    pub fn Debug(data: *const u8);
-    pub fn strlen(p: *const u8) -> u32;
+    pub fn Debug(data: *const u8, len: u16);
+    pub fn strlen(p: *const u8) -> size_t;
 }
 
+#[warn(non_snake_case)]
 pub unsafe fn DebugS(s: String) {
-    Debug(s.as_str().as_ptr());
+    Debug(s.as_str().as_ptr(), s.len() as u16);
 }
 
 #[cfg(test)]
 use core::slice;
 
 #[cfg(test)]
-pub unsafe fn Debug(data: *const u8) {
-    let len = strlen(data) as usize;
+pub unsafe fn Debug(data: *const u8, len: u16) {
     let s = slice::from_raw_parts(data, len)
         .iter()
         .map(|e| *e as char)
